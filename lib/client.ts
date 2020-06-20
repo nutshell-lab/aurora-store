f23a7184eeb2f459bb449b5cb9ccb47ab9136113
cprice <cprice@tutanota.com>
2020-06-20 19:10:21 +0200

    Add a SLS_ENV switch to change DB source

--- lib/client.ts
+++ lib/client.ts
@@ -5,6 +5,25 @@ import { migrations as config } from '../knexfile'
 
 const context = fs.readdirSync(path.join(__dirname, '../', config.directory))
 
+const envSwitch = {
+  local: {
+    host: process.env.LOCAL_DB_HOST,
+    port: Number.parseInt(process.env.LOCAL_DB_PORT),
+    database: process.env.LOCAL_DB_NAME,
+    user: process.env.LOCAL_DB_USER,
+    password: process.env.LOCAL_DB_PASSWD
+  },
+  default: {
+    host: process.env.DB_HOST,
+    port: Number.parseInt(process.env.DB_PORT),
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWD
+  }
+}
+
+const dbEnv = () => process.env.SLS_STAGE === 'local' ? envSwitch.local : envSwitch.default
+
 const migrationSource = {
     getMigrations() {
       return Promise.resolve(context)
@@ -21,11 +40,11 @@ const connectToDb = (options?: Knex.Config) =>
   Knex({
     client: 'pg',
     connection: {
-      host: process.env.DB_HOST,
-      port: Number.parseInt(process.env.DB_PORT),
-      database: process.env.DB_NAME,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWD
+      host: dbEnv().host,
+      port: dbEnv().port,
+      database: dbEnv().database,
+      user: dbEnv().user,
+      password: dbEnv().password
     },
     ...options
   })
