acaaa8c200ebc4ba74cea9c7b4d49ca1d5a85b9f
cprice <cprice@tutanota.com>
2020-06-20 23:18:48 +0200

    unversion dist

--- dist/client.d.ts
+++
@@ -1,4 +0,0 @@
-import Knex from 'knex';
-export declare const migrate: () => Promise<void>;
-declare const _default: (options?: Knex.Config) => Promise<Knex<any, unknown[]>>;
-export default _default;
